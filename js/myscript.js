3a3ca5b6a26ed08c885cad8da6cea44bd1a960ae
mariusz79 <mpawlowic@gmail.com>
2018-10-28 17:21:52 +0000

    changing colors of odd and even elements

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
     $("th").addClass("underline");  //underline all h2 elements
     $("tr").addClass("border");     //adds a border to all ul elements
+    $("tr:nth-child(odd)").addClass("selection");
+    $("tr:nth-child(even)").addClass("selectionLight");
+
 })
